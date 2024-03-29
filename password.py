01f077340da3d1417ccb459f1070d887df77a843
jialin <wao1204080218@gmail.com>
2024-03-29 23:19:29 +0800

    basic version done

---
+++ password.py
@@ -0,0 +1,14 @@
+password = 'a123456'
+
+x = 3
+
+while True :
+	user = input('請輸入密碼：')
+	if user == password:
+		print('登入成功！')
+		break
+	else:
+		x = x - 1
+		print('密碼錯誤！還有',x,'次機會')
+		if x == 0:
+			break
